16bec2ad0dc2ae3f671b592235fcc28ec9035cae
Marius THORRE Portable <marius.thorre@etu.univ-amu.fr>
2024-06-12 11:12:05 +0200

    Resolve path conflits in README file

--- graph_matching/demos/demo_display_graph.py
+++ graph_matching/demos/demo_display_graph.py
@@ -5,14 +5,16 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-
-project_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
-sys.path.append(project_root)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(script_dir, '..', '..'))
+if project_root not in sys.path:
+    sys.path.append(project_root)
 
 import graph_matching.utils.graph.display_graph_tools as display
 
-graph = display.get_graph_from_pickle("../demos/graph_generated/test/reference_test.gpickle")
+pickle_path = os.path.join(script_dir, "graph_generated", "pickle", "reference.gpickle")
+
+graph = display.get_graph_from_pickle(pickle_path)
 
 window = display.Visualisation(graph, title="Graph reference 0")
 window.display()
