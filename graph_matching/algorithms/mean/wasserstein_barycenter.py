7dc7c73cf437a6db71553a40c905aa4e67deb65b
Marius THORRE <77981546+Morpheus5828@users.noreply.github.com>
2024-06-25 16:44:41 +0200

    Refactore and test wasserstein_barycenter computation

--- graph_matching/algorithms/mean/wasserstein_barycenter.py
+++ graph_matching/algorithms/mean/wasserstein_barycenter.py
@@ -22,299 +22,55 @@ class Barycenter:
             self,
             graphs: list,
             nb_node: int,
-            find_tresh_inf: float,
-            find_tresh_sup: float,
-            find_tresh_step: int,
-            graph_vmin: float,
-            graph_vmax: float,
-            graph_title: str = "Barycenter"
+            title: str = "Barycenter"
     ):
         """
         Compute graphs barycenter
         :param graphs: list of graphs
         :param nb_node: barycenter node number that we want
-        :param find_tresh_inf: param for window
-        :param find_tresh_sup: param for window
-        :param find_tresh_step: param for window
-        :param graph_vmin: param for window
-        :param graph_vmax: param for window
         :param graph_title: graph title
         """
         self.graphs = graphs
         self.size_bary = nb_node
-        self.graph_title = graph_title
-        self.find_tresh_inf = find_tresh_inf
-        self.find_tresh_sup = find_tresh_sup
-        self.find_tresh_step = find_tresh_step
-        self.graph_vmin = graph_vmin
-        self.graph_vmax = graph_vmax
-        self.C, self.A = self.compute()
+        self.graph_title = title
+        self.F, self.A = None, None
 
-    def find_thresh(self) -> tuple:
-        dist = []
-        search = np.linspace(self.find_tresh_inf, self.find_tresh_sup, self.find_tresh_step)
-        for thresh in search:
-            Cprime = self.sp_to_adjacency(0, thresh)
-            SC = shortest_path(Cprime, method='D')
-            SC[SC == float('inf')] = 100
-            dist.append(np.linalg.norm(SC - self.C))
-        return search[np.argmin(dist)], dist
-
-    def sp_to_adjacency(
-            self,
-            threshinf: float = 0.2,
-            threshsup: float = 1.8
-    ) -> np.ndarray:
-        H = np.zeros_like(self.C)
-        np.fill_diagonal(H, np.diagonal(self.C))
-        C = self.C - H
-        C = np.minimum(np.maximum(C, threshinf), threshsup)
-        C[C == threshsup] = 0
-        C[C != 0] = 1
-        return C
-
-    def graph_colors(self, nx_graph) -> list:
-        cnorm = mcol.Normalize(vmin=self.graph_vmin, vmax=self.graph_vmax)
-        cpick = cm.ScalarMappable(norm=cnorm, cmap='viridis')
-        cpick.set_array([])
-        val_map = {}
-        for k, v in nx.get_node_attributes(nx_graph, 'attr_name').items():
-            val_map[k] = cpick.to_rgba(v)
-        colors = []
-        for node in nx_graph.nodes():
-            colors.append(val_map[node])
-        return colors
-
-    def get_attributes(self) -> tuple:
-        Ys = []
-        Cs = []
-
+    def compute(self) -> None:
+        adj_matrices = []
+        nodes_positions = []
         for graph in self.graphs:
-            coord = []
-            for n in range(len(graph.nodes)):
-                coord.append(np.array(graph.nodes[n]["coord"]))
-            Ys.append(coord)
-            Cs.append(nx.adjacency_matrix(graph).todense())
+            adj_matrices.append(nx.adjacency_matrix(graph).todense())
+            nodes = []
+            for index in range(len(graph.nodes)):
+                nodes.append(graph.nodes[index]["coord"])
 
-        return Cs, Ys
+            nodes_positions.append(np.array(nodes))
+        self.F, self.A = ot.gromov.fgw_barycenters(
+            N=3,
+            Ys=nodes_positions,
+            Cs=adj_matrices,
+            alpha=0.5,
+            fixed_structure=True,
+            init_C=adj_matrices[0]
+        )
 
-    def compute(self) -> tuple:
-        Cs, Ys = self.get_attributes()
-        #C, A = ot.gromov.fgw_barycenters(N=self.size_bary, Ys=Ys, Cs=Cs, alpha=1)
-        C, A = self.fgw_barycenters(self.size_bary, Ys, Cs, alpha=0.5)
-        return C, A
-
-    def fgw_barycenters(
-            self,
-            N, Ys, Cs, ps=None, lambdas=None, alpha=0.5, fixed_structure=False,
-            fixed_features=False, p=None, loss_fun='square_loss', armijo=False,
-            symmetric=True, max_iter=100, tol=1e-9, stop_criterion='barycenter',
-            warmstartT=False, verbose=False, log=False, init_C=None, init_X=None,
-            random_state=None, **kwargs
-    ) -> tuple:
-        """
-        Function extracted from ot.gromov from POT python package
-        :param N:
-        :param Ys:
-        :param Cs:
-        :param ps:
-        :param lambdas:
-        :param alpha:
-        :param fixed_structure:
-        :param fixed_features:
-        :param p:
-        :param loss_fun:
-        :param armijo:
-        :param symmetric:
-        :param max_iter:
-        :param tol:
-        :param stop_criterion:
-        :param warmstartT:
-        :param verbose:
-        :param log:
-        :param init_C:
-        :param init_X:
-        :param random_state:
-        :param kwargs:
-        :return:
-        """
-        if stop_criterion not in ['barycenter', 'loss']:
-            raise ValueError(f"Unknown `stop_criterion='{stop_criterion}'`. Use one of: {'barycenter', 'loss'}.")
-
-        Cs = list_to_array(*Cs)
-        Ys = list_to_array(*Ys)
-        arr = [*Cs, *Ys]
-        if ps is not None:
-            arr += list_to_array(*ps)
-        else:
-            ps = [unif(C.shape[0], type_as=C) for C in Cs]
-        if p is not None:
-            arr.append(list_to_array(p))
-        else:
-            p = unif(N, type_as=Cs[0])
-
-        nx = get_backend(*arr)
-
-        S = len(Cs)
-        if lambdas is None:
-            lambdas = [1. / S] * S
-
-        d = Ys[0].shape[1]
-
-        if fixed_structure:
-            if init_C is None:
-                raise UndefinedParameter('If C is fixed it must be initialized')
-            else:
-                C = init_C
-        else:
-            if init_C is None:
-                generator = check_random_state(random_state)
-                xalea = generator.randn(N, 2)
-                C = dist(xalea, xalea)
-                C = nx.from_numpy(C, type_as=ps[0])
-            else:
-                C = init_C
-
-        if fixed_features:
-            if init_X is None:
-                raise UndefinedParameter('If X is fixed it must be initialized')
-            else:
-                X = init_X
-        else:
-            if init_X is None:
-                X = nx.zeros((N, d), type_as=ps[0])
-
-            else:
-                X = init_X
-
-        Ms = [dist(X, Ys[s]) for s in range(len(Ys))]
-
-        if warmstartT:
-            T = [None] * S
-
-        cpt = 0
-
-        if stop_criterion == 'barycenter':
-            inner_log = False
-            err_feature = 1e15
-            err_structure = 1e15
-            err_rel_loss = 0.
-
-        else:
-            inner_log = True
-            err_feature = 0.
-            err_structure = 0.
-            curr_loss = 1e15
-            err_rel_loss = 1e15
-
-        if log:
-            log_ = {}
-            if stop_criterion == 'barycenter':
-                log_['err_feature'] = []
-                log_['err_structure'] = []
-                log_['Ts_iter'] = []
-            else:
-                log_['loss'] = []
-                log_['err_rel_loss'] = []
-
-        while ((err_feature > tol or err_structure > tol or err_rel_loss > tol) and cpt < max_iter):
-            if stop_criterion == 'barycenter':
-                Cprev = C
-                Xprev = X
-            else:
-                prev_loss = curr_loss
-
-            # get transport plans
-            if warmstartT:
-                res = [fused_gromov_wasserstein(
-                    Ms[s], C, Cs[s], p, ps[s], loss_fun=loss_fun, alpha=alpha, armijo=armijo, symmetric=symmetric,
-                    G0=T[s], max_iter=max_iter, tol_rel=1e-5, tol_abs=0., log=inner_log, verbose=verbose, **kwargs)
-                    for s in range(S)]
-            else:
-                res = [fgw.conditional_gradient(
-                    C1=C,
-                    C2=Cs[s],
-                    mu_s=p,
-                    mu_t=ps[s],
-                    distance=Ms[s],
-                    gamma=0.005,
-                    eta=20,
-                    rho=0.1,
-                    N1=100,
-                    N2=50,
-                    ot_method="sinkhorn",
-                    tolerance=1e-5
-                ) for s in range(S)]
-                #print(res)
-
-                # sinkhorn 0.3, tolerance 1e-5
-                # res = [fused_gromov_wasserstein(
-                #     Ms[s], C, Cs[s], p, ps[s], loss_fun=loss_fun, alpha=alpha, armijo=armijo, symmetric=symmetric,
-                #     G0=None, max_iter=max_iter, tol_rel=1e-5, tol_abs=0., log=inner_log, verbose=verbose, **kwargs)
-                #     for s in range(S)]
-            if stop_criterion == 'barycenter':
-                T = res
-            else:
-                T = [output[0] for output in res]
-                curr_loss = np.sum([output[1]['fgw_dist'] for output in res])
-
-            # update barycenters
-            if not fixed_features:
-                Ys_temp = [y.T for y in Ys]
-                X = update_feature_matrix(lambdas, Ys_temp, T, p, nx).T
-                Ms = [dist(X, Ys[s]) for s in range(len(Ys))]
-
-            if not fixed_structure:
-                if loss_fun == 'square_loss':
-                    C = update_square_loss(p, lambdas, T, Cs, nx)
-
-                elif loss_fun == 'kl_loss':
-                    C = update_kl_loss(p, lambdas, T, Cs, nx)
-
-            # update convergence criterion
-            if stop_criterion == 'barycenter':
-                err_feature, err_structure = 0., 0.
-                if not fixed_features:
-                    err_feature = nx.norm(X - Xprev)
-                if not fixed_structure:
-                    err_structure = nx.norm(C - Cprev)
-                if log:
-                    log_['err_feature'].append(err_feature)
-                    log_['err_structure'].append(err_structure)
-                    log_['Ts_iter'].append(T)
-
-                if verbose:
-                    if cpt % 200 == 0:
-                        print('{:5s}|{:12s}'.format(
-                            'It.', 'Err') + '\n' + '-' * 19)
-                    print('{:5d}|{:8e}|'.format(cpt, err_structure))
-                    print('{:5d}|{:8e}|'.format(cpt, err_feature))
-            else:
-                err_rel_loss = abs(curr_loss - prev_loss) / prev_loss if prev_loss != 0. else np.nan
-                if log:
-                    log_['loss'].append(curr_loss)
-                    log_['err_rel_loss'].append(err_rel_loss)
-
-                if verbose:
-                    if cpt % 200 == 0:
-                        print('{:5s}|{:12s}'.format(
-                            'It.', 'Err') + '\n' + '-' * 19)
-                    print('{:5d}|{:8e}|'.format(cpt, err_rel_loss))
-
-            cpt += 1
-
-        if log:
-            log_['T'] = T
-            log_['p'] = p
-            log_['Ms'] = Ms
-
-            return X, C, log_
-        else:
-            return X, C
-
-    def get_graph(self):
+    def get_graph(self) -> nx.Graph:
         G = nx.from_numpy_array(self.A)
         tmp = nx.Graph()
         for node, i in enumerate(G.nodes):
-            tmp.add_node(node, coord=self.C[i], label=node)
+            tmp.add_node(node, coord=self.F[i], label=node)
         return tmp
+
+    def fwg_barycenter(self, N, Ys, Cs, alpha):
+        Cs = list_to_array(*Cs)
+        Ys = list_to_array(*Ys)
+        arr = [*Cs, *Ys]
+        ps = [unif(C.shape[0], type_as=C) for C in Cs]
+        p = unif(N, type_as=Cs[0])
+
+        n = get_backend(*arr)
+        S = len(Cs)
+        lambdas = [1. / S] * S
+        d = Ys[0].shape[1]
+        C = None
+
--- test/algorithms/mean/test_wasserstein_barycenter.py
+++ test/algorithms/mean/test_wasserstein_barycenter.py
@@ -1,87 +1,113 @@
 import os, sys
 
 from unittest import TestCase
-import math
-import networkx as nx
+
 import numpy as np
+import networkx as nx
 
-from graph_matching.algorithms.mean import wasserstein_barycenter
-from graph_matching.utils.display_graph_tools import get_graph_from_pickle
-
+from graph_matching.algorithms.mean.wasserstein_barycenter import Barycenter
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_path = os.path.abspath(os.path.join(current_dir, '../../..'))
 if project_path not in sys.path:
     sys.path.append(project_path)
 
+g1 = nx.Graph()
+g1.add_node(0, coord=np.array([0, 0]))
+g1.add_node(1, coord=np.array([0, 1]))
+g1.add_node(2, coord=np.array([0, 2]))
+g1.add_edge(0, 1)
+g1.add_edge(1, 2)
 
+g2 = nx.Graph()
+g2.add_node(0, coord=np.array([1, 0]))
+g2.add_node(1, coord=np.array([1, 1]))
+g2.add_node(2, coord=np.array([1, 2]))
+g2.add_edge(0, 1)
+g2.add_edge(1, 2)
 
+g3 = nx.Graph()
+g3.add_node(0, coord=np.array([1, 0]))
+g3.add_node(1, coord=np.array([1, 1]))
+g3.add_node(2, coord=np.array([1, 2]))
+g3.add_edge(0, 1)
+g3.add_edge(1, 2)
 
-def build_noisy_circular_graph(N=20, mu=0, sigma=0.3, with_noise=False, structure_noise=False, p=None):
-    """ Create a noisy circular graph
-    """
-    g = nx.Graph()
-    g.add_nodes_from(list(range(N)))
-    for i in range(N):
-        noise = float(np.random.normal(mu, sigma, 1))
-        if with_noise:
-            g.add_node(i, attr_name=math.sin((2 * i * math.pi / N)) + noise)
-        else:
-            g.add_node(i, attr_name=math.sin(2 * i * math.pi / N))
-        g.add_edge(i, i + 1)
-        if structure_noise:
-            randomint = np.random.randint(0, p)
-            if randomint == 0:
-                if i <= N - 3:
-                    g.add_edge(i, i + 2)
-                if i == N - 2:
-                    g.add_edge(i, 0)
-                if i == N - 1:
-                    g.add_edge(i, 1)
-    g.add_edge(N, 0)
-    noise = float(np.random.normal(mu, sigma, 1))
-    if with_noise:
-        g.add_node(N, attr_name=math.sin((2 * N * math.pi / N)) + noise)
-    else:
-        g.add_node(N, attr_name=math.sin(2 * N * math.pi / N))
-    return g
+g4 = nx.Graph()
+g4.add_node(0, coord=np.array([1, 0]))
+g4.add_node(1, coord=np.array([1, 1]))
+g4.add_node(2, coord=np.array([1, 2]))
+g4.add_node(3, coord=np.array([1, 3]))
+g4.add_edge(0, 1)
+g4.add_edge(1, 2)
+g4.add_edge(2, 3)
 
 
 class TestWassersteinBarycenter(TestCase):
-    def testCompute(self):
-        graphs = []
-        #for k in range(9):
-        #    X0.append(build_noisy_circular_graph(np.random.randint(15, 25), with_noise=True, structure_noise=True, p=3))
-        # plt.figure(figsize=(8, 10))
-        # for i in range(len(X0)):
-        #     plt.subplot(3, 3, i + 1)
-        #     g = X0[i]
-        #     pos = nx.kamada_kawai_layout(g)
-        #     nx.draw(g, pos=pos, node_color=graph_colors(g, vmin=-1, vmax=1), with_labels=False, node_size=100)
-        # plt.suptitle('Dataset of noisy graphs. Color indicates the label', fontsize=20)
-        # plt.show()
-        pickle_path = os.path.join(project_path, "graph_matching/demos/graph_generated/pickle")
-
-        for item in os.listdir(pickle_path):
-            item_path = os.path.join(pickle_path, item)
-            if os.path.isdir(item_path):
-                for graph_file in os.listdir(item_path):
-                    graph_path = os.path.join(item_path, graph_file)
-                    graph = get_graph_from_pickle(graph_path)
-                    graphs.append(graph)
-
-        w = wasserstein_barycenter.Barycenter(
-            graphs=graphs,
-            size_bary=30,
-            find_tresh_inf=0.5,
-            find_tresh_step=10,
-            find_tresh_sup=10,
-            graph_vmax=1,
-            graph_vmin=-1,
+    def test_compute(self):
+        b = Barycenter(
+            graphs=[g1, g2],
+            nb_node=3
         )
-        # print(w.log.keys())
-        # print(w.log["Ms"][0].shape)
-        w.plot_middle_graph()
 
+        b.compute()
+        truth_f = np.array([
+            [0.5, 2.0],
+            [0.5, 1],
+            [0.5, 0]
+        ])
+
+        truth_a = np.array([
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ])
+
+        self.assertTrue(np.array_equal(truth_f, b.F))
+        self.assertTrue(np.array_equal(truth_a, b.A))
+
+        b = Barycenter(
+            graphs=[g1, g2, g3],
+            nb_node=3
+        )
+
+        b.compute()
+
+        truth_f = np.array([
+            [0.66666667, 2.0],
+            [0.66666667, 1.0],
+            [0.66666667, 0.0]
+        ])
+
+        truth_a = np.array([
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ])
+
+        self.assertTrue(np.allclose(truth_f, b.F))
+        self.assertTrue(np.array_equal(truth_a, b.A))
+
+        b = Barycenter(
+            graphs=[g1, g2, g3, g4],
+            nb_node=3
+        )
+
+        b.compute()
+
+        truth_f = np.array([
+            [0.75, 2.1875],
+            [0.75, 1.125],
+            [0.75, 0.0625]
+        ])
+
+        truth_a = np.array([
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ])
+
+        self.assertTrue(np.allclose(truth_f, b.F))
+        self.assertTrue(np.array_equal(truth_a, b.A))
 
 
 
