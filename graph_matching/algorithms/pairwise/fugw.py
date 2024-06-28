690e1d2bae2628d81454f438923195e06ff3985f
Marius THORRE <77981546+Morpheus5828@users.noreply.github.com>
2024-06-28 10:47:59 +0200

    Starting implementation of fugw

---
+++ graph_matching/algorithms/pairwise/fugw.py
@@ -0,0 +1,7 @@
+"""This module contains a matching method between a pair of graph
+Implementation of Thual, A., Tran, Q. H., Zemskova, T., Courty, N., Flamary, R. et al.
+Aligning individual brains with fused unbalanced Gromov Wasserstein
+Advances in neural information processing systems, 35, 21792-21804
+
+.. moduleauthor:: Marius Thorre
+"""
